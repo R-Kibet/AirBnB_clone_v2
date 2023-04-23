73df70ec039210cb5e4f5db73c1dd40339b44d0e
root <richkibz@gmail.com>
2023-04-23 19:47:32 +0300

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" rendering html showing states """
+""" Script that runs an app with Flask framework """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -9,20 +9,19 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def tear(exception):
-    """ Tears down the session """
+def teardown_session(exception):
+    """ Teardown """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def states():
+def display_html():
     """ Function called with /states_list route """
     states = storage.all(State)
-    dct = {value.id: value.name for value in states.values()}
+    dict_to_html = {value.id: value.name for value in states.values()}
     return render_template('7-states_list.html',
                            Table="States",
-                           items=dct)
+                           items=dict_to_html)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
