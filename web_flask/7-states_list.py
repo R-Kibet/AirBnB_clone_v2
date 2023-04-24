085dd68e5e89c18f785d846ce2e8554eca112a88
root <richkibz@gmail.com>
2023-04-24 06:22:00 +0300

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" rendering html showing states """
+""" rendering html showing states of the module"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
